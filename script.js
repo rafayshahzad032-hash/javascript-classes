65c7bfdc3a5bc4c1e4e066d5cfa4c5c329ca7828
rafayshahzad032-hash <rafayshahzad032@gmail.com>
2025-10-31 20:33:57 +0500

    Added class 1 JavaScript work

---
+++ script.js
@@ -0,0 +1,8 @@
+alert('Welcome to our website!')
+
+alert('Error! pls enter a valid password.')
+
+alert('Welcome to JS Land... \nHappy Coding!')
+
+alert('Welcome to JS Land...')
+alert('Happy Coding!')
